f1ae48cd74204b0d7f02b1462825e6030cc68197
AmparoClavijo <amparo.0014@hotmail.com>
2022-08-17 23:18:56 -0500

    coloque una nueva linea

--- src/main/java/com/mycompany/ciclo3prueba/Ciclo3Prueba.java
+++ src/main/java/com/mycompany/ciclo3prueba/Ciclo3Prueba.java
@@ -11,8 +11,8 @@ package com.mycompany.ciclo3prueba;
  */
 public class Ciclo3Prueba {
     public static void main(String[] args){
-       System.out.println("Hello World");
-       
+       System.out.println("Hello World");       
+       System.out.println("Otra linea de codigo");
     }
     
 }
